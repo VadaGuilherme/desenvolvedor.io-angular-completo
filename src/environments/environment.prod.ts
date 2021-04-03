0ee51245542b35a8346ad13a607424bcea0fe88c
Guilherme Vada <gvadaguariba@gmail.com>
2021-04-02 22:49:01 -0300

    adding dockerfile and nginx

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrlv1: 'https://localhost:5001/api/v1/',
-  imagensUrl: 'https://localhost:5001/'
+  apiUrlv1: 'http://infosystembrasil.ddns.net:5021/api/v1/',
+  imagensUrl: 'http://infosystembrasil.ddns.net:5021/'
 };
